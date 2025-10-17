b5539373b40854fca5cfd8adefad5b96a98ac25a
section002 <section002fk@gmail.com>
2025-10-17 19:00:08 +0900

    ログイン画面のUI作成_途中

--- src/main/java/jp/co/actec/attendance/controller/LoginController.java
+++ src/main/java/jp/co/actec/attendance/controller/LoginController.java
@@ -3,8 +3,8 @@ package jp.co.actec.attendance.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class LoginController {
@@ -15,9 +15,9 @@ public class LoginController {
         return "login";
     }
 
-    @RequestMapping(value = "/login", method = RequestMethod.GET)
-   public String login(/*@RequestParam("username") String username,
-                       @RequestParam("password") String password,*/
+    @PostMapping("/login")
+   public String login(@RequestParam("username") String username,
+                       @RequestParam("password") String password,
                        Model model) {
        return "redirect:/registration";
     }
